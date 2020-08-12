9bb7a09517b70de9e650824e7845f3576543066b
ITDSIU19053 <69240867+ITDSIU19053@users.noreply.github.com>
2020-08-12 16:56:18 +0800

    Update Exercise1.cpp

--- Exercise1.cpp
+++ Exercise1.cpp
@@ -1,26 +1,27 @@
-/*
-1. Given an array of string, find the longest common prefix of all element.
- ________________________________________________
-| Input: abc abd abf                             |
-| Output: ab                                     |
-|________________________________________________|
-*/
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
-#include<limits.h>
-#define SIZE 1000
-
-void ex1(char *str[],int n){
-	//your codes here
+#include<bits/stdc++.h>
+using namespace std;
+string matchedPrefixtill(string s1, string s2) {
+   string res;
+   int n1 = s1.length(); //store length of string s1.
+   int n2 = s2.length(); //store length of string s2.
+   for (int i = 0, j = 0; i <= n1 - 1 && j <= n2 - 1; i++, j++) {    
+      if (s1[i] != s2[j])
+         break;
+      res.push_back(s1[i]);
+   }
+   return (res);
 }
-
-int main(int argc, char *argv[]) {
-	
-	char *str[]={"abc","abcd","abe","abdr"};
-	ex1(str,4);
-			
-	return 0;
+string matchedPrefix (string a[], int n) {
+   string pre = a[0];
+   for (int i = 1; i <= n - 1; i++)
+   pre = matchedPrefixtill(pre, a[i]);
+   return (pre);
+}
+int main() {
+   string a[] = {"abc","abcd","abe","abdr"}; //taking inputs
+   int n = sizeof(a) / sizeof(a[0]);
+   string res = matchedPrefix(a, n);
+   if (res.length())
+      cout<< res.c_str();
+   return (0);
 }
